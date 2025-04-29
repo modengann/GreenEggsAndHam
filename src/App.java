16a228d77cb6201b4adf92bdafeee59c6227b7c5
modengann <99417989+modengann@users.noreply.github.com>
2025-04-29 12:22:18 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/*
+*Your mission is to write a small Java program that proves whether Green Eggs and Ham 
+*really uses only 50 different words. You’ll start with two things: an empty Main class 
+and a text file that contains the entire story. Your job is to read the file line by 
+line and use appropriate skills to see count the number of words. How you do this is up to you!
+*/
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+}
